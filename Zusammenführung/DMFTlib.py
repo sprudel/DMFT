fe4de1a36ad63b327eaa94f51eeb942699778a0e
Hannes Herrmann <hannes.herrmann@physik.uni-muenchen.de>
2016-02-11 00:28:01 +0100

    improved calc of g_loc and Padé aprrox

--- Zusammenführung/DMFTlib.py
+++ Zusammenführung/DMFTlib.py
@@ -69,6 +69,10 @@ def self_energy(G0_omega,U):
 # ## Local Greensfunction
 # compute local Green's function from self energy given a disperion / density of states.
 # Here we use dispersion relation of Bethe lattice.
+# $$G_{loc}(iω) = \int\limits_{-D}^{D} dε \frac{ρ(ε)}{iω-ε-\Sigma(iω)} \quad ρ(ε)= \frac{2}{π D} \sqrt{1-\frac{ε^2}{D^2}}$$
+# This can be calculated analytically (Mathematica):
+# $$G_{loc}(iω) = \frac{2}{π D} \left( B π + \sqrt{1-B^2}\left[\log{(1-B)}-\log{(B-1)}\right] \right)$$
+# with $B=\frac{iω-\Sigma(iω)}{D}$ and only for $\Im{B} \neq 0 $ and $-1\leq\Re{B}\leq1$.
 
 # In[5]:
 
@@ -80,10 +84,12 @@ def dos_bethe(e):
 def Gloc_omega(self_energy_omega):
     #we use same resultion of energies as for matsubara frequencies
     N = self_energy_omega.shape[0]
-    energies_bethe = np.linspace(-D,D,N)
-    denergies_bethe = energies_bethe[2] - energies_bethe[1]
+    #energies_bethe = np.linspace(-D,D,N)
+    #denergies_bethe = energies_bethe[2] - energies_bethe[1]
     freq = matsubara_freq(N)
-    return simps( dos_bethe(energies_bethe)/(1j*freq[:,np.newaxis]-energies_bethe-self_energy_omega[:,np.newaxis]) ,dx=denergies_bethe)
+    B=(1j*freq-self_energy_omega)/D
+    return 2/np.pi/D*(B*np.pi+np.sqrt(1-B**2)*(np.log(1-B)-np.log(B-1)))
+    #return simps( dos_bethe(energies_bethe)/(1j*freq[:,np.newaxis]-energies_bethe-self_energy_omega[:,np.newaxis]) ,dx=denergies_bethe)
 
 
 # ## DMFT Loop
@@ -109,10 +115,10 @@ def DMFT_loop(G0_initial_omega,U,iterations=1,frac_new=1.):
 # i.e.
 # $$G(-i ω) = G(i ω)^*$$
 
-# In[7]:
+# In[1]:
 
 class PadeApproximation:
-    def __init__(self, points, values,use_every=1):
+    def __init__(self, points, values,cut_freq=1.,use_every=1):
         
         #first sort frequencies (points = i omega), sort can also sort complex arrays (real first, then complex)
         #np.arg gives index to sort array
@@ -120,8 +126,8 @@ class PadeApproximation:
         self.points = points[index_sorted]
         self.values = values[index_sorted]
         
-        #only positive freqencies and only use every [use_every] points
-        index_pos = np.imag(self.points)>0
+        #only positive freqencies below cat and only use every [use_every] points
+        index_pos = np.logical_and(np.imag(self.points)>0,np.imag(self.points)<cut_freq*np.imag(self.points).max())
         self.points = self.points[index_pos][::use_every]
         self.values = self.values[index_pos][::use_every]
         
@@ -132,15 +138,15 @@ class PadeApproximation:
         for i in range(1,self.N):
             g_matrix[i,i:] = ( g_matrix[i-1,i-1] - g_matrix[i-1,i:] ) / ( ( self.points[i:] - self.points[i-1] ) * g_matrix[i-1,i:] )
         self.a_coeff = np.diag(g_matrix)
-    def __call__(self,z):
+    def __call__(self,z,norm=1.):
         positive_imag_index = np.imag(z)>0
         tmp = np.zeros_like(z)
         #for positive imaginary index do normal procedure
-        tmp[positive_imag_index] = self._evaluate_fit(z[positive_imag_index])
+        tmp[positive_imag_index] = self._evaluate_fit(z[positive_imag_index],norm=norm)
         #for negative imaginary index use symmetriy
-        tmp[np.logical_not(positive_imag_index)] = np.conjugate(self._evaluate_fit(-z[np.logical_not(positive_imag_index)]))
+        tmp[np.logical_not(positive_imag_index)] = np.conjugate(self._evaluate_fit(-z[np.logical_not(positive_imag_index)],norm=norm))
         return tmp
-    def _evaluate_fit(self,z):
+    def _evaluate_fit_old(self,z):
         Nz = z.shape[0]
         A = np.zeros((self.N+1,Nz),dtype=np.complex256)
         B = np.zeros((self.N+1,Nz),dtype=np.complex256)
@@ -151,4 +157,30 @@ class PadeApproximation:
             A[i] = A[i-1] + (z-self.points[i-2]) * self.a_coeff[i-1] * A[i-2]
             B[i] = B[i-1] + (z-self.points[i-2]) * self.a_coeff[i-1] * B[i-2]
         return A[self.N]/B[self.N]
+    def _evaluate_fit(self,z,norm=1.):
+        Nz = z.shape[0]
+        Am1 = np.zeros_like(z,dtype=np.complex256)
+        Am2 = np.zeros_like(z,dtype=np.complex256)
+        Bm1 = np.ones_like(z,dtype=np.complex256)
+        Bm2 = np.ones_like(z,dtype=np.complex256)
+        tmpA = np.zeros_like(z,dtype=np.complex256)
+        tmpB = np.zeros_like(z,dtype=np.complex256)
+        factor = np.zeros_like(z,dtype=np.complex256)
+        np.copyto(Am1, self.a_coeff[0])
+        for i in range(2,self.N+1):
+            np.copyto(factor,(z-self.points[i-2])* self.a_coeff[i-1])
+            #normalisation = 1/np.sqrt(1+np.abs(factor)**2-np.sqrt(1+1/4*np.abs(factor)**4))
+            np.copyto(tmpA,norm*Am1)
+            np.copyto(Am1, norm*(Am1 + factor* Am2))
+            np.copyto(Am2,tmpA)
+            np.copyto(tmpB,norm*Bm1)
+            np.copyto(Bm1, norm*(Bm1 + factor* Bm2))
+            np.copyto(Bm2,tmpB)
+        return Am1/Bm1
+        
+
+
+# In[ ]:
+
+
 
